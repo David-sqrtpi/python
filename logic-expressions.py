822e92f7a0b5276b45e3fd8df64cf024a87b2149
David-sqrtpi <david.stiven911@gmail.com>
2021-06-03 00:08:19 -0500

    Initial commit

---
+++ logic-expressions.py
@@ -0,0 +1,14 @@
+print("Write a number, please ")
+
+number = input()
+
+print("Write another number, please ")
+
+another_number = input()
+
+print("Results... ")
+
+if number == another_number:
+    print("Numbers you just entered are equal")
+else:
+    print("Numbers you just entered are non-equal")
