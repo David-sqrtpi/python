cc79797a2d7bbc0d199bd38eb858cb08636d2152
David-sqrtpi <david.stiven911@gmail.com>
2021-06-04 00:39:09 -0500

    favorite city challenge

---
+++ final-fundamental-challenge.py
@@ -0,0 +1,8 @@
+def favorite_city(name):
+    return name
+
+city = input("What is your favorite city?: ")
+
+print("One of my favorite cities is", favorite_city(city))
+print("Your favorite city is", favorite_city(city))
+print("Don't forget that my favorite city is", favorite_city(city))
